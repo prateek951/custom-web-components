a9213ec54b39f5e3c6dc8d201ad3d47a88465531
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2019-01-08 14:11:08 +0530

    using slots and writing templates inside js

--- Tooltip-Web-Component/tooltip.js
+++ Tooltip-Web-Component/tooltip.js
@@ -8,28 +8,36 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._ToolTipContainer;
-    this._ToolTipText = 'Some dummy tooltip text.'
-  }
+    this._ToolTipText = "Some dummy tooltip text.";
+    //custom element shadow dom
+    this.attachShadow({ mode: "open" });
+    // const template = document.querySelector("#tooltip-template");
+    this.shadowRoot.innerHTML = `<slot>Some default</slot>
+        <span>(?)</span>`;
+}
   connectedCallback() {
     //attach element to the dom
-    if(this.hasAttribute('markContent')) {
-        this._ToolTipText = this.getAttribute('markContent');
+    if (this.hasAttribute("markContent")) {
+      this._ToolTipText = this.getAttribute("markContent");
     }
-    
-    const ToolTipIcon = document.createElement("span");
-    ToolTipIcon.textContent = `(?)`;
+    const ToolTipIcon = this.shadowRoot.querySelector('span');
     ToolTipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     ToolTipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(ToolTipIcon);
+    this.shadowRoot.appendChild(ToolTipIcon);
+    this.style.position = "relative";
   }
   //private method
   _showTooltip() {
     this._ToolTipContainer = document.createElement("div");
     this._ToolTipContainer.textContent = this._ToolTipText;
-    this.appendChild(this._ToolTipContainer);
+    this._ToolTipContainer.style.backgroundColor = "black";
+    this._ToolTipContainer.style.color = "white";
+    this._ToolTipContainer.style.position = "absolute";
+    this._ToolTipContainer.style.zIndex = "10";
+    this.shadowRoot.appendChild(this._ToolTipContainer);
   }
   _hideTooltip() {
-    this.removeChild(this._ToolTipContainer);
+    this.shadowRoot.removeChild(this._ToolTipContainer);
   }
 }
 
